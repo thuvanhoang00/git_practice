a393c31af5a38680a1b4c716a5eb593325a04c7a
thuvanhoang00 <thu.hoangvan00@gmail.com>
2025-10-31 20:07:29 -0700

    master lv2_1

--- level2/main.cpp
+++ level2/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <thread>
+
+void print(){
+    std::cout << "threadid: " << std::this_thread::get_id() << " Hello world\n";
+}
 
 int main(){
-
+    std::thread t(print);
+    t.join();
     return 0;
 }
